33279e06f75f0a294b33820bb5eebb45cebcc495
chung tri <nhattri.uit@gmail.com>
2025-02-06 21:12:28 +0700

    rabbitMQ message queue

---
+++ src/tests/message_queue/rabbitmq/consumer.js
@@ -0,0 +1,30 @@
+const amqplib = require('amqplib');
+
+const message = 'hello ,RabbitMq for Tips javascript'
+
+const runConsumer = async () => {
+    try {
+        const connection = await amqplib.connect('amqp://localhost');
+        const channel = await connection.createChannel();
+
+        const queueName = 'test-topic';
+        await channel.assertQueue(queueName, { 
+            durable: true // khi server crash vấn đề gi đó , khi start lại rabbitmq thì nó sẽ gửi lại message chứ ko mất dữ liệu
+         });
+
+         // send message to consumer channel
+         channel.consume(queueName, (message) => {
+            console.log(`Received ${message.content.toString()}`)
+         }, {
+            noAck: true // nếu dữ lieu đã gửi roi thì ko xử lý nữa
+         });
+
+
+
+         console.log(`message sent : `, message);
+    }catch(e) {
+        console.log(e)
+    }
+}
+
+runConsumer().catch(console.error)
